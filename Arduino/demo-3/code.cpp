62b4b16cadd38868103e2ca5d42c47fafdb4ff4f
CyberX <samuelajala01@gmail.com>
2022-03-02 16:20:46 +0100

    Update code.cpp

--- Arduino/demo-3/code.cpp
+++ Arduino/demo-3/code.cpp
@@ -1,1 +1,34 @@
 
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  Serial.println(0.01723 * readUltrasonicDistance(3, 2));
+  if (0.01723 * readUltrasonicDistance(3, 2) < 40) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500); // Wait for 500 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  Serial.println("");
+}
