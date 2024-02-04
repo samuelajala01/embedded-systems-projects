f356e16139be1b99118d9390ebe682eabfdcbf58
Sam <samuelajala01@gmail.com>
2024-02-04 13:13:07 +0100

    Add files via upload

---
+++ Arduino/dht-test.ino
@@ -0,0 +1,46 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-dht11
+ */
+
+#include <DHT11.h>
+#define DHT11_PIN 2
+
+DHT11 dht11(DHT11_PIN, DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  dht11.begin(); // initialize the sensor
+}
+
+void loop() {
+  // wait a few seconds between measurements.
+  delay(2000);
+
+  // read humidity
+  float humi  = dht11.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht11.readTemperature();
+  // read temperature as Fahrenheit
+  float tempF = dht11.readTemperature(true);
+
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT11 sensor!");
+  } else {
+    Serial.print("DHT11# Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  "); 
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C ~ ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+}
